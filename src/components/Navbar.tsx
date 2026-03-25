fdd315b058644d2891f61fae7c1890aa4c7879c5
jmijmijmijmi <88359863+jmijmijmijmi@users.noreply.github.com>
2026-03-25 17:04:07 +0900

    Feature: Added About page and updated Navbar navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,9 @@ export default function Navbar() {
     <nav className="glass">
       <div className="container nav-content">
         <Link href="/" className="logo">JUNG CHIYOON</Link>
+        <ul className="nav-links">
+          <li><Link href="/about">ABOUT</Link></li>
+        </ul>
       </div>
     </nav>
   );
